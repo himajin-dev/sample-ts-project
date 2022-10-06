c82233a400c043318777316747b555956519e524
Kenta Okamoto <chocoby@himajin.dev>
2022-10-06 15:19:28 +0900

    Import sub packages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { hello } from "@himajin-dev/sample-npm-package";
 
-console.log("Hello from TS project!")
+// FIXME: I want to import a sub packages without `dist`.
+//   However, it is difficult to combine with TypeScript, so I gave up.
+import { foo } from "@himajin-dev/sample-npm-package/dist/foo";
+import { baz } from "@himajin-dev/sample-npm-package/dist/bar/baz";
 
-hello("Sample")
+hello("from TS Project!")
+foo("foo!")
+baz("baz!")
