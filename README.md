ccd3f9af8e8174bff10b28db856f0da50e06cc55
Kenta Okamoto <chocoby@himajin.dev>
2022-10-10 08:22:17 +0900

    Import packages without `dist`

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ This is a sample TypeScript project using npm packages hosted at GitHub Packages
 
 Used sample packages:
 
-- [`@himajin-dev/sample-npm-package`](https://github.com/orgs/himajin-dev/packages/npm/package/sample-npm-package)
 - [`@himajin-dev/sample-monorepo-npm-package-foo`](https://github.com/orgs/himajin-dev/packages/npm/package/sample-monorepo-npm-package-foo)
 - [`@himajin-dev/sample-monorepo-npm-package-bar`](https://github.com/orgs/himajin-dev/packages/npm/package/sample-monorepo-npm-package-bar)
 - [`@himajin-dev/sample-monorepo-npm-package-baz`](https://github.com/orgs/himajin-dev/packages/npm/package/sample-monorepo-npm-package-baz)
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import { hello } from "@himajin-dev/sample-npm-package";
-
-// FIXME: I want to import a sub packages without `dist`.
-//   However, it is difficult to combine with TypeScript, so I gave up.
-import { foo } from "@himajin-dev/sample-npm-package/dist/foo";
-import { baz } from "@himajin-dev/sample-npm-package/dist/bar/baz";
-
 import { foo as monorepoFoo } from "@himajin-dev/sample-monorepo-npm-package-foo";
 import { bar as monorepoBar } from "@himajin-dev/sample-monorepo-npm-package-bar";
 
-// Use re-exported module to avoid including `dist` in the path
-import * as monorepoBaz from "@himajin-dev/sample-monorepo-npm-package-baz";
-
-hello("from TS Project!")
-foo("foo!")
-baz("baz!")
+import { baz } from "@himajin-dev/sample-monorepo-npm-package-baz";
+import { foo } from "@himajin-dev/sample-monorepo-npm-package-baz/foo";
+import { bar } from "@himajin-dev/sample-monorepo-npm-package-baz/foo/bar";
 
 monorepoFoo("foo!?")
 monorepoBar("bar!?")
 
-monorepoBaz.baz("baz!?")
-monorepoBaz.foo("baz-foo!?")
-monorepoBaz.bar("baz-foo-bar!?")
+baz("baz")
+foo("baz-foo")
+bar("baz-foo-bar")
